c9bc6284a72013eebad8605130f22e9fd0cfc24b
surfer <jpjpsingh7@gmail.com>
2020-01-28 23:30:21 +0530

    welcome attached

--- app/src/main/java/com/example/robocore/MainActivity.java
+++ app/src/main/java/com/example/robocore/MainActivity.java
@@ -1,21 +1,36 @@
 package com.example.robocore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        cardView=findViewById(R.id.cardView);
+
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back= new Intent(MainActivity.this,welcome.class);
+                startActivity(back);
+                finish();
+            }
+        });
 
     }
 }
