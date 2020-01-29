56cf995efe0932f019eee420f00a983435dfeb8c
surfer <jpjpsingh7@gmail.com>
2020-01-29 20:57:15 +0530

    updated

--- app/src/main/java/com/example/robocore/MainActivity.java
+++ app/src/main/java/com/example/robocore/MainActivity.java
@@ -9,10 +9,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 private CardView cardView;
+private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +23,21 @@ private CardView cardView;
 
 
         cardView=findViewById(R.id.cardView);
-
+        register=findViewById(R.id.textView2);
 
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent back= new Intent(MainActivity.this,welcome.class);
+                startActivity(back);
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back= new Intent(MainActivity.this,register.class);
                 startActivity(back);
                 finish();
             }
--- app/src/main/java/com/example/robocore/welcome.java
+++ app/src/main/java/com/example/robocore/welcome.java
@@ -22,9 +22,9 @@ public class welcome extends AppCompatActivity {
         smalltobig= AnimationUtils.loadAnimation(this,R.anim.smalltobig);
         nothingtocome=AnimationUtils.loadAnimation(this,R.anim.nothingtocome);
         btnanim=AnimationUtils.loadAnimation(this,R.anim.btnanim);
-        imageview=(ImageView)findViewById(R.id.imageView);
-        textview=(TextView)findViewById(R.id.textView);
-        btn=(Button)findViewById(R.id.btn);
+        imageview=findViewById(R.id.imageview);
+        textview=findViewById(R.id.textview);
+        btn=findViewById(R.id.btn);
         imageview.startAnimation(smalltobig);
         textview.startAnimation(nothingtocome);
         btn.startAnimation(btnanim);
