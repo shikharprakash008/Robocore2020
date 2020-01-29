5f6dcd969485a96810d8b6e2db4c393101109c07
aman20119 <amansourav5@gmail.com>
2020-01-30 00:20:20 +0530

    bhaiya ye pehla screen h bataye kuch changes krna h toh

---
+++ app/src/main/java/com/example/robocore/splashscreen.java
@@ -0,0 +1,58 @@
+package com.example.robocore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class splashscreen extends AppCompatActivity {
+
+    Animation topanim,bottomanim;
+    ImageView image;
+    TextView logo;
+    ProgressBar progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        topanim= AnimationUtils.loadAnimation(this,R.anim.splashanimtop);
+        bottomanim= AnimationUtils.loadAnimation(this,R.anim.splashanimbottom);
+        image = findViewById(R.id.corsitlogo);
+        logo = findViewById(R.id.splashcorsit);
+        progress = findViewById(R.id.splashprogress);
+        image.setAnimation(topanim);
+        logo.setAnimation(bottomanim);
+        progress.setAnimation(bottomanim);
+        final Intent intent = new Intent(this,welcome.class);
+Thread timer = new Thread(){
+    public void run(){
+        try{
+
+
+            sleep(3000);
+        }catch (InterruptedException e){
+
+            e.printStackTrace();
+        }
+        finally {
+            startActivity(intent);
+            finish();
+        }
+
+
+
+
+    }
+};
+
+
+timer.start();
+
+    }
+}
