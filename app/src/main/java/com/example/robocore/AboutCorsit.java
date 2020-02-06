27d362fe6d7780b58d33b45809c94e520dc02a19
aman20119 <amansourav5@gmail.com>
2020-02-06 22:02:41 +0530

    updated robocre and corsit

--- app/src/main/java/com/example/robocore/AboutCorsit.java
+++ app/src/main/java/com/example/robocore/AboutCorsit.java
@@ -1,14 +1,34 @@
 package com.example.robocore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutCorsit extends AppCompatActivity {
-
+ImageView image2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_corsit);
+        image2=findViewById(R.id.back);
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMain();
+            }
+        });
+
+    }
+    public  void openMain(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+
+
+
+
     }
 }
