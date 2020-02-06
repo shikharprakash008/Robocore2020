5f5b44acc6b5b4a696a80ede1730a2ea4ac0c421
aman20119 <amansourav5@gmail.com>
2020-02-07 01:59:48 +0530

    updated robocre and corsit

--- app/src/main/java/com/example/robocore/AboutCorsit.java
+++ app/src/main/java/com/example/robocore/AboutCorsit.java
@@ -6,14 +6,26 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class AboutCorsit extends AppCompatActivity {
 ImageView image2;
+private RecyclerView recyclerview;
+private RecyclerView recyclerview2;
+private int[] images= {R.drawable.logo,R.drawable.instagram
+};
+private RecyclerAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_corsit);
         image2=findViewById(R.id.back);
+        recyclerview=findViewById(R.id.recyclerView);
+        recyclerview2=findViewById(R.id.recyclerView2);
+        adapter=new RecyclerAdapter(images);
+        recyclerview.setAdapter(adapter);
+        recyclerview2.setAdapter(adapter);
         image2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ app/src/main/java/com/example/robocore/RecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.robocore;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Imageviewholder> {
+    private  int[] images;
+    public RecyclerAdapter(int[] images){
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public Imageviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_layout,parent,false);
+        Imageviewholder imageviewholder = new Imageviewholder(view);
+        return imageviewholder;
+    }
+
+    @Override
+    public void onBindViewHolder(Imageviewholder holder, int position) {
+int images_id=images[position];
+holder.item.setImageResource(images_id);
+holder.itemtext.setText("Image:"+position);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public  static class Imageviewholder extends RecyclerView.ViewHolder
+    {
+ImageView item;
+TextView itemtext;
+
+        public Imageviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            item = itemView.findViewById(R.id.imagerecycle);
+itemtext =itemView.findViewById(R.id.textrecycle);
+
+        }
+    }
+
+}
